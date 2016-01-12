4299b815645aa6a19ec8459502a9fd4cbfff8e79
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-12 14:06:25 -0500

    Finish set up

--- board.rb
+++ board.rb
@@ -1,19 +1,36 @@
 class Board
   # sets the value of the rows and columns the game board will have.
-  def setSize
+
+  def set_rows
     puts "How many rows?"
     @rows = gets.chomp.to_i
+    if @rows <= 0
+      puts "Row number can't be 0 or less"
+      set_rows
+    end
+  end
+
+  def set_columns
     puts "How many columns?"
     @columns = gets.chomp.to_i
+    if @columns <= 0
+      puts "Column number can't be 0 or less"
+      set_columns
+    end
+  end
+
+  def set_board_size
+    set_rows
+    set_columns
   end
 
   # Creates the game board based on @rows and @columns
-  def createBoard
+  def create_board
     @board = Array.new(@rows) {Array.new(@columns, "_")}
   end
 
   # Formats the @board array so it looks prettier
-  def showBoard
+  def show_board
     colNum = 0
     @columns.times do
       print "#{colNum} "
@@ -31,9 +48,9 @@ class Board
   end
 
   def initialize
-    setSize
-    createBoard
-    showBoard
+    set_board_size
+    create_board
+    show_board
   end
 
 end
--- game.rb
+++ game.rb
@@ -1,3 +1,56 @@
 require_relative 'board'
 
-Board.new
+
+class Game
+
+  def initialize
+    new_game
+  end
+
+  #Prompts the player to select between X or O
+  def player_select
+    puts "Select X or O"
+    choice = gets.chomp.upcase
+    if choice == "X" || choice == "O"
+      @player_choice = choice
+      puts "Player chose #{@player_choice}"
+      sleep 1
+      computer_choice
+    else
+      puts "Invalid selection, try again."
+      player_select
+    end
+  end
+
+  # The computer will play whatever the player won't
+  def computer_choice
+    if @player_choice == "X"
+      @computer_choice = "O"
+      puts "Computer will play #{@computer_choice}"
+    else
+      @computer_choice = "X"
+      puts "Computer will play #{@computer_choice}"
+    end
+  end
+
+  # checks if player wants to play again
+  def play_again
+    puts "Want to play again?"
+    answer = gets.chomp.upcase
+    if answer == "Y"
+      new_game
+    else
+      puts "Thanks for playing, come back again!"
+    end
+  end
+
+  # starts a new game
+  def new_game
+    Board.new
+    player_select
+    play_again
+  end
+
+end
+
+Game.new
